5be40dfe42188335a6651fa33940308ecef4ca9f
rmorey16 <moreyrachel@gmail.com>
2021-01-27 22:03:20 -0700

    Add project files.

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morey__Assignment2.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GradeCalculator ()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/javascript.js
@@ -0,0 +1,36 @@
+﻿
+$("#calculateGrade").click(function () {
+    var iAssignmentsGrade = document.getElementById("assignments").value;
+    var iGroupProjectsGrade = document.getElementById("group_projects").value;
+    var iQuizzesGrade = document.getElementById("quizzes").value;
+    var iExamsGrade = document.getElementById("exams").value;
+    var iIntexGrade = document.getElementById("intex").value;
+
+    if (iAssignmentsGrade > 100 || iAssignmentsGrade < 0) {
+        alert("Please enter a number between 0-100")
+    }
+    else if (iGroupProjectsGrade > 100 || iGroupProjectsGrade < 0) {
+        alert("Please enter a number between 0-100")
+    }
+    else if (iQuizzesGrade > 100 || iQuizzesGrade < 0) {
+        alert("Please enter a number between 0-100")
+    }
+    else if (iExamsGrade > 100 || iExamsGrade < 0) {
+        alert("Please enter a number between 0-100")
+    }
+    else if (iIntexGrade > 100 || iIntexGrade < 0) {
+        alert("Please enter a number between 0-100")
+    }
+    else {
+        //set up some blank variables to be used later on
+        var iFinalGrade = 0;
+
+        //calculate the final grade based on grade weights and what the user inputted
+        iFinalGrade = ((iAssignmentsGrade * .5) + (iGroupProjectsGrade * .1) + (iQuizzesGrade * .1) + (iExamsGrade * .2) + (iIntexGrade * .1));
+
+        //create an output variable
+        alert('Based on the grades that you inputted, you will end up with a ' + iFinalGrade + ' in the class');
+    }
+    //alert(iAssignmentsGrade);
+    
+});
